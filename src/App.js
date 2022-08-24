92811cff270171a71e898d8afb42759da24d405c
Arman Niyaj Mogal <100214997+ArmanMogal@users.noreply.github.com>
2022-08-24 21:39:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Free from "./Free";
+import Plus from "./Plus";
+import Pro from "./Pro";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      <Free />
+      <Plus />
+      <Pro />
+    </div>
+  );
+};
+
+export default App;
